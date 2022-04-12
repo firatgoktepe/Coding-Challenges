5a7ee452d8e3e5beb1967d01bb6f5cb769dedebc
Firat Göktepe <firat@naxnordic.com>
2022-04-12 14:13:31 +0300

    added Q2 and its solutions

---
+++ 2- Mini-Max Sum/answer.js
@@ -0,0 +1,8 @@
+function miniMaxSum(arr) {
+    arr.sort((a, b) => a - b);
+
+    let min = arr.slice(0, arr.length - 1).reduce((a,b) => a+b)
+    let max = arr.slice(1).reduce((a,b) => a+b)
+
+    console.log(min, max)
+}
