bf27a812c63ea0c3ae7f3269317189e825b681ca
Firat Göktepe <firat@naxnordic.com>
2022-04-18 18:46:04 +0300

    fixed readme of max question

--- Max Consecutive Ones/answer.js
+++ Max Consecutive Ones/answer.js
@@ -1,7 +1,3 @@
-// Given a binary array nums, return the maximum number of consecutive 1's in the array.
-
-// Example: Input: [1,1,0,1,1,1,0,1] Output: 3
-
 var findMaxConsecutiveOnes = function(nums) {
   let max = 0;
   let count = 0;
